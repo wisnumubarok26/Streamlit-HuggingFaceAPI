6e49d6686a716d497cce16b363e888e2b6eedb26
unknown <wisnumubarok2002@gmail.com>
2024-02-13 16:07:56 +0700

    perbaikan env dan keyAPI

--- model.py
+++ model.py
@@ -1,12 +1,17 @@
+from dotenv import load_dotenv
+import os
 import requests
 
+# Memuat variabel lingkungan dari file .env
+load_dotenv()
+
+# Mengakses variabel lingkungan API
+API = os.getenv('API')
+
 API_URL = "https://api-inference.huggingface.co/models/StevenLimcorn/indonesian-roberta-base-emotion-classifier"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": f"Bearer {API}"}
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-
-
-
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
