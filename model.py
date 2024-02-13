9b1b7e7e3d1b28887abece594f9d8c459243f7ea
unknown <wisnumubarok2002@gmail.com>
2024-02-13 16:10:20 +0700

    balikin semula

--- model.py
+++ model.py
@@ -1,17 +1,12 @@
-from dotenv import load_dotenv
-import os
 import requests
 
-# Memuat variabel lingkungan dari file .env
-load_dotenv()
-
-# Mengakses variabel lingkungan API
-API = os.getenv('API')
-
 API_URL = "https://api-inference.huggingface.co/models/StevenLimcorn/indonesian-roberta-base-emotion-classifier"
-headers = {"Authorization": f"Bearer {API}"}
+headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+
+
 
